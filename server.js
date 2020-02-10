843d3ab0f2073940a4fce90889705a6ffa33b947
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2020-02-10 14:56:18 -0500

    handlebar directories and files added to the projecxt

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 
 const express = require('express');
-const app = express();
+var app = express();
 
 app.use(logger('dev'));
 app.use(
@@ -12,11 +12,25 @@ app.use(
         })
 );
 
-const exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.use(express.static(process.cwd() + "/public"));
 
-const PORT = process.env.PORT || 3000; 
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars");
+
+mongoose.connect("mongodb://localhost/scraped_news");
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection errorL "));
+db.once("open", function(){
+    console.log("connected to mongoose");
+});
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+var PORT = process.env.PORT || 3000; 
 app.listen(PORT, function(){
     console.log('listening on PORT ' + PORT);
 });
